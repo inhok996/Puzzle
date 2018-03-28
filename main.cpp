9be1cb2ae0de4a31fdb841874d8daf7de2473263
Unknown <inho211@naver.com>
2018-03-28 20:58:30 +0900

    전체기능 동작
    gameState, cTime 추가, indentation 수정

--- main.cpp
+++ main.cpp
@@ -15,11 +15,17 @@
 #define MOVE_U 2
 #define MOVE_D 3
 
+#define STOP 0
+#define PLAYING 1
+#define PAUSE 2
+
 #define DIM 4 //4x4 퍼즐맵의 크기
 
 static int x,y; //빈칸의 위치
 static int moveNum; //이동회수
 static clock_t sTime; //시작 시간
+static clock_t cTime; //현재 시간
+static int gameState = STOP;
 
 //게임 Map 그리기 시작 위치: 필요하다면 출력 위치를 조정할 수 있음 
 #define MAP_STARTX 20 
@@ -53,43 +59,45 @@ void swap(int di,int (*map)[DIM]);
 
 int end_condition(int(*map)[DIM]);
 int getAction(int (*m)[DIM]);//모든 퍼즐이 제 위치에 있거나 ESC키가 눌러졌으면 정수 0을 반환
-                   // 방향키를 눌렀을 때는 해당 키값을 반환(LEFT, RIGHT, DOWN, UP)
-                   //필요시에 매개변수 인자를 추가할 수 있음
+// 방향키를 눌렀을 때는 해당 키값을 반환(LEFT, RIGHT, DOWN, UP)
+//필요시에 매개변수 인자를 추가할 수 있음
 
 int getDirectKey(){
-    if(kbhit()!= 0) //키보드를 눌렀는지 확인함
-      return getch() == 224 ? getch():-1;   //특수 키를 눌렀을 때 버퍼에 2Byte가 발생함, 첫번째 값은 224값을 발생하고 두번째 값은 특수키에 따라 다름
-                                           //특수 키를 확인하기 위해 2번의 getch()함수를 호출해야 함
+	if(kbhit()!= 0) //키보드를 눌렀는지 확인함
+		return getch() == 224 ? getch():-1;   //특수 키를 눌렀을 때 버퍼에 2Byte가 발생함, 첫번째 값은 224값을 발생하고 두번째 값은 특수키에 따라 다름
+	//특수 키를 확인하기 위해 2번의 getch()함수를 호출해야 함
 } 
 
 
 int main(void)
 {
-   int map[DIM][DIM];
-   int action = 1;
-   int moved = 1;
-   clock_t cTime; //현재시간
+	int map[DIM][DIM];
+	int action = 1;
+	int moved = 1;
 
-   init(map);
-   shuffle(10,map);
-   gameDraw(map,moved);
-   action = getAction(map);
-   sTime = clock();
-   while(action)
-   {
-      puzzleMove(map,action);
-      gameDraw(map,moved);
-	  action = getAction(map);
-   }
+	init(map);
+	shuffle(10,map);
+	gameDraw(map,moved);
+	action = getAction(map);
+	sTime = clock();
+	gameState = PLAYING;
+	while(action)
+	{
+		cTime = clock();
+		puzzleMove(map,action);
+		gameDraw(map,moved);
+		action = getAction(map);
+	}
+	gameState = STOP;
 
-   return 0;
+	return 0;
 }
 
 //콘솔 화면에서 커서를 특정 위치로 이동
 void gotoXY(int x, int y) 
 {
- COORD Pos = {x, y};
- SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+	COORD Pos = {x, y};
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
 
 // 그외 함수들을 정의함
@@ -109,10 +117,10 @@ void init(int (*m)[DIM]) //초기화 함수; 필요시에 매개변수를 추가함
 void gameDraw(int (*m)[DIM],int& moved)
 {
 	if(moved == 1){ //이동이 일어났을때만 출력
-	gotoXY(MAP_STARTX,MAP_STARTY);
-	cout << "fifteen Puzzle";
-	int x = MAP_STARTX;
-	int y = MAP_STARTY + 1;
+		gotoXY(MAP_STARTX,MAP_STARTY);
+		cout << "fifteen Puzzle";
+		int x = MAP_STARTX;
+		int y = MAP_STARTY + 1;
 		gotoXY(x,y);
 		for(int i = 0 ; i < DIM ; i++){
 			for(int j = 0 ; j < DIM; j++){
@@ -126,104 +134,108 @@ void gameDraw(int (*m)[DIM],int& moved)
 		cout << "이동 회수: " << moveNum << "회";
 		gotoXY(SCORE_STARTX,SCORE_STARTY + 1);
 		cout << "소요 시간: ";
-		moved = 0;
 	}
 	gotoXY(SCORE_STARTX + 11,SCORE_STARTY + 1);
-	cout << sTime << "초";
-	
+	cout << (cTime - sTime) / (double)CLK_TCK << "초";
+
 }
 void swap(int di,int (*map)[DIM])
 {
-   int temp;
-   switch(di)
-      {
-      case MOVE_L:
-         if(x>0)
-         {
-            temp=map[y][x];
-            map[y][x]=map[y][x-1];
-            map[y][x-1]=temp;
-            x--;
-         }
-         break;
-      case MOVE_R:
-         if(x<3)
-         {
-            temp=map[y][x];
-            map[y][x]=map[y][x+1];
-            map[y][x+1]=temp;
-            x++;
-         }
-         break;
-      case MOVE_U:
-         if(y>0)
-         {
-            temp=map[y][x];
-            map[y][x]=map[y-1][x];
-            map[y-1][x]=temp;
-            y--;
-         }
-         break;
-      case MOVE_D:
-         if(y<3)
-         {
-            temp=map[y][x];
-            map[y][x]=map[y+1][x];
-            map[y+1][x]=temp;
-            y++;
-         }
-         break;
-      default: break;
-      }
+	int temp;
+	switch(di)
+	{
+	case MOVE_L:
+		if(x>0)
+		{
+			temp=map[y][x];
+			map[y][x]=map[y][x-1];
+			map[y][x-1]=temp;
+			x--;
+			if(gameState == PLAYING) moveNum++;
+		}
+		break;
+	case MOVE_R:
+		if(x<3)
+		{
+			temp=map[y][x];
+			map[y][x]=map[y][x+1];
+			map[y][x+1]=temp;
+			x++;
+			if(gameState == PLAYING) moveNum++;
+		}
+		break;
+	case MOVE_U:
+		if(y>0)
+		{
+			temp=map[y][x];
+			map[y][x]=map[y-1][x];
+			map[y-1][x]=temp;
+			y--;
+			if(gameState == PLAYING) moveNum++;
+		}
+		break;
+	case MOVE_D:
+		if(y<3)
+		{
+			temp=map[y][x];
+			map[y][x]=map[y+1][x];
+			map[y+1][x]=temp;
+			y++;
+			if(gameState == PLAYING) moveNum++;
+		}
+		break;
+	default: break;
+	}
+
 }
 void puzzleMove(int(*map)[DIM],int action) //퍼즐 이동 함수; 필요시에 매개변수 추가함
 {
-   int key_order=-1;
+	int key_order=-1;
 
-   if(action==LEFT) key_order=MOVE_L;
-   else if(action==RIGHT) key_order=MOVE_R;
-   else if(action==UP) key_order=MOVE_U;
-   else if(action==DOWN) key_order=MOVE_D;
+	if(action==LEFT) key_order=MOVE_L;
+	else if(action==RIGHT) key_order=MOVE_R;
+	else if(action==UP) key_order=MOVE_U;
+	else if(action==DOWN) key_order=MOVE_D;
 
-   swap(key_order, map);
+	swap(key_order, map);
 }
 
 void shuffle(int num, int(*map)[DIM])//퍼즐 맵 초기화; 50회 무작위 이동, 필요시에 인자 추가 할 수 있음
 {
-   int direction;
-   int moved = 1;
-   srand((unsigned int)time(NULL));
-   for(int i=0;i<num;i++)
-   {
-      gameDraw(map,moved);
-      Sleep(100);
-      direction = rand() % 4;
-      swap(direction,map);
-   }
+	int direction;
+	int moved = 1;
+	srand((unsigned int)time(NULL));
+	for(int i=0;i<num;i++)
+	{
+		gameDraw(map,moved);
+		Sleep(100);
+		direction = rand() % 4;
+		swap(direction,map);
+	}
 }
 
 int end_condition(int(*map)[DIM])
 {
-   int val=0;
-   for(int i=0;i<DIM;i++)
-      for(int j=0;j<DIM;j++)
-         {
-            if(map[i][j]==val) val++;
-            else return 0;
-         }
-   return val;
+	int val=0;
+	for(int i=0;i<DIM;i++)
+		for(int j=0;j<DIM;j++)
+		{
+			if(map[i][j]==val) val++;
+			else return 0;
+		}
+		return val;
 }
 
 int getAction(int(*map)[DIM])
 {
-   int key;   
-   if(end_condition(map)) return 0;
-   key = getDirectKey();
-   //printf("key = %d\n",key);
-   if((key == LEFT) || (key == RIGHT) || (key == DOWN) || (key == UP) || (key == ESC));
-   else key = -1;
-   if(key == ESC) return 0;
-   return key;
+	int key;   
+	if(end_condition(map)) return 0;
+	key = getDirectKey();
+	//printf("key = %d\n",key);
+	if((key == LEFT) || (key == RIGHT) || (key == DOWN) || (key == UP) || (key == -1));
+	else key = -2;
+	if(key == -1) return 0;
+	return key;
 }//모든 퍼즐이 제 위치에 있거나 ESC키가 눌러졌으면 정수 0을 반환
-                   // 방향키를 눌렀을 때는 해당 키값을 반환(LEFT, RIGHT, DOWN, UP)
-                   //필요시에 매개변수 인자를 추가할 수 있음
+// 방향키를 눌렀을 때는 해당 키값을 반환(LEFT, RIGHT, DOWN, UP)
+//필요시에 매개변수 인자를 추가할 수 있음
